cbaa9d5972901ca003a118a88bd0761afc62b095
David Inoa <davidinoa@gmail.com>
2023-11-22 23:29:04 -0500

    Update trpc server file and replace deprecated Clerk middleware

--- src/server/api/routers/boards.ts
+++ src/server/api/routers/boards.ts
@@ -1,32 +1,29 @@
-import { z } from 'zod'
+import { currentUser } from '@clerk/nextjs'
+import { TRPCError } from '@trpc/server'
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc'
 
 const boardsRouter = createTRPCRouter({
-  // Add a procedure to get all boards for a user including nested data
-  getAllBoardsForUser: publicProcedure
-    .input(z.object({ userId: z.string().min(1) })) // Validate input as a string with at least one character
-    .query(async ({ ctx, input }) =>
-      // Use a Prisma query to fetch boards with nested relations
-      ctx.db.board.findMany({
-        where: {
-          userId: input.userId, // Filter boards by userId provided in input
-        },
-        include: {
-          columns: {
-            include: {
-              tasks: {
-                include: {
-                  subtasks: true, // Include subtasks nested under tasks
-                },
+  getFirst: publicProcedure.query(async ({ ctx }) => {
+    const userData = await currentUser()
+    const userId = userData?.id
+    if (!userId) {
+      throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR' })
+    }
+    return ctx.db.board.findFirst({
+      where: { userId },
+      include: {
+        columns: {
+          include: {
+            tasks: {
+              include: {
+                subtasks: true,
               },
             },
           },
         },
-        orderBy: {
-          createdAt: 'desc', // Optional: Order by creation date if required
-        },
-      }),
-    ),
+      },
+    })
+  }),
 })
 
 export default boardsRouter
