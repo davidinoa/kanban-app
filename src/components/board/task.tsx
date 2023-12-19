03217e4def089cd49faa0df7963eadf2789f2dfd
David Inoa <davidinoa@gmail.com>
2023-12-18 22:39:22 -0400

    style(task-card): improve drag handle scaling while pressing the card

--- src/components/board/task.tsx
+++ src/components/board/task.tsx
@@ -3,6 +3,7 @@
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 import { Card, CardBody } from '@nextui-org/card'
+import { useState } from 'react'
 import DragIcon from '~/assets/icon-drag.svg'
 import useAppStore from '~/zustand/app-store'
 
@@ -28,6 +29,7 @@ export default function Task({
 
   const board = useAppStore((state) => state.currentBoard)
   const setViewingTaskId = useAppStore((state) => state.setViewingTaskId)
+  const [isPressing, setIsPressing] = useState(false)
 
   if (!board) return null
 
@@ -56,6 +58,8 @@ export default function Task({
         <Card
           isPressable
           onPress={() => setViewingTaskId(taskId)}
+          onPressStart={() => setIsPressing(true)}
+          onPressEnd={() => setIsPressing(false)}
           classNames={{
             body: 'p-0 flex flex-col gap-2',
             base: `px-4 py-6 pr-8 w-full relative`,
@@ -75,9 +79,9 @@ export default function Task({
         </Card>
         <button
           type="button"
-          className={`${
-            displayOverlay ? 'cursor-grabbing' : 'cursor-grab'
-          } absolute right-2 top-1/2 z-10 translate-y-[-50%]`}
+          className={`${displayOverlay ? 'cursor-grabbing' : 'cursor-grab'} ${
+            isPressing ? 'scale-90' : 'scale-100'
+          } absolute right-2 top-6 z-10 transition-all`}
           {...attributes}
           {...listeners}
         >
