1b53aa1f5719071f350f00a2a256946fcef4bfc0
David Inoa <davidinoa@gmail.com>
2023-12-23 20:05:01 -0400

    refactor: remove suspense boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import {
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import { cookies } from 'next/headers'
-import { Suspense, type PropsWithChildren } from 'react'
+import { type PropsWithChildren } from 'react'
 import { Toaster } from 'react-hot-toast'
 import ClerkLoadingPage from '~/components/clerk-loading-page'
 import Layout from '~/components/layout'
@@ -33,24 +33,22 @@ export default function RootLayout({ children }: PropsWithChildren) {
       <body suppressHydrationWarning>
         <ClerkProvider>
           <TRPCReactProvider cookies={cookies().toString()}>
-            <Suspense fallback="Loading...">
-              <Providers>
-                <SignedIn>
-                  <Layout>{children}</Layout>
-                </SignedIn>
-              </Providers>
-              <SignedOut>
-                <ClerkLoading>
-                  <ClerkLoadingPage />
-                </ClerkLoading>
-                <ClerkLoaded>{children}</ClerkLoaded>
-              </SignedOut>
-              <Toaster
-                toastOptions={{
-                  className: 'dark:bg-gray-200 dark:text-white',
-                }}
-              />
-            </Suspense>
+            <Providers>
+              <SignedIn>
+                <Layout>{children}</Layout>
+              </SignedIn>
+            </Providers>
+            <SignedOut>
+              <ClerkLoading>
+                <ClerkLoadingPage />
+              </ClerkLoading>
+              <ClerkLoaded>{children}</ClerkLoaded>
+            </SignedOut>
+            <Toaster
+              toastOptions={{
+                className: 'dark:bg-gray-200 dark:text-white',
+              }}
+            />
           </TRPCReactProvider>
         </ClerkProvider>
         <SpeedInsights />
